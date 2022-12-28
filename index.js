4e449afe6404ee861b5abc0123b3dd3e3f6e4891
satoshi-okayama-ringzero <satoshi.okayama@ringzero.co.jp>
2022-12-28 10:15:06 +0900

    launchtypeを上に

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ async function run() {
     try {
       const commandList = parseCommand(command);
       taskResponse = await ecs.runTask({
+        launchType: "FARGATE",
         //capacityProviderStrategy: serviceResponse.capacityProviderStrategy,
         cluster: cluster,
         taskDefinition: taskDefinition,
